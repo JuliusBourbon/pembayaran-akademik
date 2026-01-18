4f94f6006501a1f143b3a7d3610c0d7a2d3fac9e
JuliusBourbon <naherrrrr@gmail.com>
2026-01-18 19:41:02 +0700

    delete files

--- routes/web.php
+++ routes/web.php
@@ -13,41 +13,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/table', [TableController::class, 'read'], function () {
-    return view('table');
-});
-Route::get('/table/tambahprodi', [ProdiController::class, 'createView'])->name('prodi.createview');
-Route::post('/table/tambahprodi', [ProdiController::class, 'store'])->name('prodi.store');
-Route::delete('/table/hapusprodi/{id}', [ProdiController::class, 'delete'])->name('prodi.delete');
-Route::get('/table/ubahprodi/{id}', [ProdiController::class, 'updateView'])->name('prodi.updateview');
-Route::post('/table/ubahprodi/{id}', [ProdiController::class, 'update'])->name('prodi.update');
-
-Route::get('/table/tambahfakul', [FakultasController::class, 'createView'])->name('fakul.createview');
-Route::post('/table/tambahfakul', [FakultasController::class, 'store'])->name('fakul.store');
-Route::delete('/table/hapusfakul/{id}', [FakultasController::class, 'delete'])->name('fakul.delete');
-Route::get('/table/ubahfakul/{id}', [FakultasController::class, 'updateView'])->name('fakul.updateview');
-Route::post('/table/ubahfakul/{id}', [FakultasController::class, 'update'])->name('fakul.update');
-
-Route::get('/table/tambahmhs', [MahasiswaController::class, 'createView'])->name('mhs.createview');
-Route::post('/table/tambahmhs', [MahasiswaController::class, 'store'])->name('mhs.store');
-Route::delete('/table/hapusmhs/{id}', [MahasiswaController::class, 'delete'])->name('mhs.delete');
-Route::get('/table/ubahmhs/{id}', [MahasiswaController::class, 'updateView'])->name('mhs.updateview');
-Route::post('/table/ubahmhs/{id}', [MahasiswaController::class, 'update'])->name('mhs.update');
-
-Route::get('/table/tambahbdh', [BendaharaController::class, 'createView'])->name('bdh.createview');
-Route::post('/table/tambahbdh', [BendaharaController::class, 'store'])->name('bdh.store');
-Route::delete('/table/hapusbdh/{id}', [BendaharaController::class, 'delete'])->name('bdh.delete');
-Route::get('/table/ubahbdh/{id}', [BendaharaController::class, 'updateView'])->name('bdh.updateview');
-Route::post('/table/ubahbdh/{id}', [BendaharaController::class, 'update'])->name('bdh.update');
-
-Route::get('/table/tambahjenis', [JenisController::class, 'createView'])->name('jenis.createview');
-Route::post('/table/tambahjenis', [JenisController::class, 'store'])->name('jenis.store');
-Route::delete('/table/hapusjenis/{id}', [JenisController::class, 'delete'])->name('jenis.delete');
-Route::get('/table/ubahjenis/{id}', [JenisController::class, 'updateView'])->name('jenis.updateview');
-Route::post('/table/ubahjenis/{id}', [JenisController::class, 'update'])->name('jenis.update');
-
-Route::get('/table/tambahpby', [PembayaranContoller::class, 'createView'])->name('pby.createview');
-Route::post('/table/tambahpby', [PembayaranContoller::class, 'store'])->name('pby.store');
-Route::delete('/table/hapuspby/{id}', [PembayaranContoller::class, 'delete'])->name('pby.delete');
-Route::get('/table/ubahpby/{id}', [PembayaranContoller::class, 'updateView'])->name('pby.updateview');
-Route::post('/table/ubahpby/{id}', [PembayaranContoller::class, 'update'])->name('pby.update');
+// Route::get('/table', [TableController::class, 'read'], function () {
+//     return view('table');
+// });
+// Route::get('/table/tambahprodi', [ProdiController::class, 'createView'])->name('prodi.createview');
+// Route::post('/table/tambahprodi', [ProdiController::class, 'store'])->name('prodi.store');
+// Route::delete('/table/hapusprodi/{id}', [ProdiController::class, 'delete'])->name('prodi.delete');
+// Route::get('/table/ubahprodi/{id}', [ProdiController::class, 'updateView'])->name('prodi.updateview');
+// Route::post('/table/ubahprodi/{id}', [ProdiController::class, 'update'])->name('prodi.update');
